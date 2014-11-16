3a1927276ba6778cdef9c09fcf7f97254175ba5a
basio <khalefam@gmail.com>
2014-11-16 18:39:44 +0200

    a

---
+++ SQLParser/sqlfunc.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sqlparser
+{
+    class column_keyvalue
+    {
+        public string name;
+        public string type;
+        public bool isnull;
+        public bool isindex;
+        public bool isdefault;
+        public string def;
+    }
+    class SQLFunc
+    {
+
+        static public void _create(string table, List<column_keyvalue> a)
+        {
+        }
+        static public void _select(string table, List<string> columnlist, List<int> condition, List<string> l, List<string> op, List<string> r)
+        {
+
+        }
+        static public void _drop(List<string> tablelist)
+        {
+        }        
+        static public void _update(string table, Dictionary<string, string> col_table, List<int> condition, List<string> l, List<string> op, List<string> r)
+        {
+        }
+        static public void _insert(string table, List<string> columns, List<string> values)
+        {
+        }
+        static public void _delete(string table, List<int> condition, List<string> l, List<string> op, List<string> r)
+        {
+        }
+
+    }
+    /*
+
+
+
+*/
+
+}
+
---
+++ testSQLParser/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using sqlparser;
+namespace testSQLParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SQLScanner scanner = new SQLScanner("d.sql");
+            SQLParser parser = new SQLParser(scanner);
+            //parser.tab = new SymbolTable(parser);
+            //parser.gen = new CodeGenerator();
+            parser.Parse();
+            if (parser.errors.count == 0)
+            {
+                //parser.gen.Decode();
+                //parser.gen.Interpret("Taste.IN");
+
+            }
+            else
+            {
+            //    foreach (var e in parser.errors)
+              //  {
+                //    Console.WriteLine(e);
+                //}
+            }
+        }
+    }
+}
